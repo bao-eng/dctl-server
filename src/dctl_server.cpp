ede812c753b73cc667d46fbe2fc1df7f044ddd58
Andrey Basov <dev.basov@gmail.com>
2022-01-02 17:53:11 +0300

    initial state for collision testing

--- src/dctl_server.cpp
+++ src/dctl_server.cpp
@@ -25,7 +25,7 @@ const float kSpeed = (float)58 / 6;
 int main() {
   SetConfigFlags(FLAG_MSAA_4X_HINT);
   InitWindow((kMapWidth + 1) * kScale, (kMapHeight + 1) * kScale, "snake");
-  SetTargetFPS(60);
+  SetTargetFPS(100);
 
   uint32_t sequence{0};
   double t = 0.0;
@@ -33,20 +33,28 @@ int main() {
   auto current_time = std::chrono::system_clock::now();
   double accumulator = 0.0;
 
-  // Game game(kMapWidth, kMapHeight, kScale, kSpeed, dt, 60);
-
   State st;
   st.sequence = sequence;
   sequence++;
 
-  Vector2 p1{3, ((float)kMapHeight / 2)+10};
-  Snake s1{0, {p1, p1}, Dir::kRight, (Color){0, 255, 255, 255}};  // CYAN
+  Vector2 p1{3, ((float)kMapHeight / 2) + 10};
+  Snake s1{0,
+           {p1,
+            {p1.x + 10, p1.y},
+            {p1.x + 10, p1.y + 10},
+            {p1.x + 5, p1.y + 10},
+            {p1.x + 5, p1.y + 10}},
+           Dir::kUp,
+           (Color){0, 255, 255, 255}};  // CYAN
   Vector2 p2{kMapWidth - 3, (float)kMapHeight / 2};
   Snake s2{1, {p2, p2}, Dir::kLeft, (Color){255, 0, 255, 255}};  // MAGENTA
   Vector2 p3{(float)kMapWidth / 2, 3};
-  Snake s3{2, {p3, p3}, Dir::kDown, (Color){0, 255, 0, 255}};  // GREEN
-  Vector2 p4{(float)kMapWidth / 2, kMapHeight - 3};
-  Snake s4{3, {p4, p4}, Dir::kUp, (Color){255, 255, 0, 255}};  // YELLOW
+  Snake s3{2,
+           {{p2.x - 10, p2.y - 10}, {p2.x - 10, p2.y - 10}},
+           Dir::kDown,
+           (Color){0, 255, 0, 255}};  // GREEN
+  Vector2 p4{10 + (float)kMapWidth / 2, kMapHeight - 3};
+  Snake s4{3, {p4, p4}, Dir::kRight, (Color){255, 255, 0, 255}};  // YELLOW
 
   st.snakes.push_back(s1);
   st.snakes.push_back(s2);
@@ -58,18 +66,18 @@ int main() {
 
   while (!WindowShouldClose())  // Detect window close button or ESC key
   {
-    Input inp{sequence,0,false,false,false,false};
+    Input inp{sequence, 0, false, false, false, false};
     game.ProcessInput(inp);
-    inp.player_id=1;
+    inp.player_id = 1;
     game.ProcessInput(inp);
-    inp.player_id=2;
+    inp.player_id = 2;
     game.ProcessInput(inp);
-    inp.player_id=3;
+    inp.player_id = 3;
     game.ProcessInput(inp);
     sequence++;
     BeginDrawing();
 
-    Draw(game.GetState(),kMapWidth,kMapHeight,kScale);
+    Draw(game.GetState(), kMapWidth, kMapHeight, kScale);
 
     DrawFPS(10, 10);
     EndDrawing();
@@ -78,8 +86,4 @@ int main() {
   CloseWindow();
 
   return 0;
-
-
-
-  
 }
