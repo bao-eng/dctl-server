88f1f8a5e929a03dd557c7d3b19a980c2e260912
Andrey Basov <dev.basov@gmail.com>
2022-01-07 03:02:34 +0300

    working synchronous udp server

--- src/dctl_server.cpp
+++ src/dctl_server.cpp
@@ -15,7 +15,7 @@
 #include "raylib.h"
 #include "raymath.h"
 
-// using boost::asio::ip::udp;
+using boost::asio::ip::udp;
 
 const size_t kMapWidth = 107;
 const size_t kMapHeight = 54;
@@ -37,14 +37,10 @@ int main() {
   st.sequence = sequence;
   sequence++;
 
-  Vector2 p1{3, ((float)kMapHeight / 2) + 10};
+  Vector2 p1{3, ((float)kMapHeight / 2)};
   Snake s1{0,
-           {p1,
-            {p1.x + 10, p1.y},
-            {p1.x + 10, p1.y + 10},
-            {p1.x + 5, p1.y + 10},
-            {p1.x + 5, p1.y + 10}},
-           Dir::kUp,
+           {p1,p1},
+           Dir::kRight,
            (Color){0, 255, 255, 255}};  // CYAN
   Vector2 p2{kMapWidth - 3, (float)kMapHeight / 2};
   Snake s2{1, {p2, p2}, Dir::kLeft, (Color){255, 0, 255, 255}};  // MAGENTA
@@ -57,23 +53,29 @@ int main() {
   Snake s4{3, {p4, p4}, Dir::kRight, (Color){255, 255, 0, 255}};  // YELLOW
 
   st.snakes.push_back(s1);
-  st.snakes.push_back(s2);
-  st.snakes.push_back(s3);
-  st.snakes.push_back(s4);
+  // st.snakes.push_back(s2);
+  // st.snakes.push_back(s3);
+  // st.snakes.push_back(s4);
 
   DCTLEngine game(st, kMapWidth, kMapHeight, kSpeed, dt, 60);
 
+  boost::asio::io_service io_service_;
+  udp::socket socket_(io_service_, udp::endpoint(udp::v4(), 7777));
+
   while (!WindowShouldClose())  // Detect window close button or ESC key
   {
-    Input inp{sequence, 0, false, false, false, false};
-    game.ProcessInput(inp);
-    inp.player_id = 1;
-    game.ProcessInput(inp);
-    inp.player_id = 2;
-    game.ProcessInput(inp);
-    inp.player_id = 3;
-    game.ProcessInput(inp);
-    sequence++;
+    boost::array<char, 64000> recv_buffer_;
+    while(socket_.available()){
+      boost::asio::ip::udp::endpoint ep;
+      auto sz = socket_.receive_from(boost::asio::buffer(recv_buffer_),ep);
+      std::vector<char> buf(sz);
+      std::copy(recv_buffer_.begin(), recv_buffer_.begin()+sz, buf.begin());
+      auto inp =UnpackInput(buf);
+      // std::cout << inp.player_id << " " << inp.sequence << inp.left << std::endl;
+      game.ProcessInput(inp);
+      auto flat = PackState(game.GetState());
+      socket_.send_to(boost::asio::buffer(flat.data(), flat.size()),ep);
+    }
     BeginDrawing();
 
     Draw(game.GetState(), kMapWidth, kMapHeight, kScale);
